6beb31f674e3b69ef1420f6128861599f7631e72
p <_>
2017-01-12 20:30:00 +0900

    Allow saving words across multiple (a few) sessions.

--- ac-last-session.el
+++ ac-last-session.el
@@ -12,6 +12,11 @@ the last session."
   :type 'string
   :group 'ac-last-session)
 
+(defcustom ac-last-session-saved-words 100000
+  "Number of words (per language) to keep in the history."
+  :type 'integer
+  :group 'ac-last-session)
+
 (defvar ac-last-session--candidates nil)
 
 (defun ac-last-session-load-saved-session ()
@@ -25,19 +30,22 @@ the last session."
 
 (defun ac-last-session-save-session ()
   (when ac-last-session-save-file
-    (let ((lst nil))
-      (dolist (buf (buffer-list))
-        (with-current-buffer buf
-          (let ((cell (assq major-mode lst))
-                (words (split-string
-                        (buffer-substring-no-properties (point-min) (point-max))
-                        "\\(?:^\\|\\_>\\).*?\\(?:\\_<\\|$\\)")))
-            (if cell (nconc (cdr cell) words) (push (cons major-mode words) lst)))))
-      (dolist (pair lst)
-        (setcdr lst (delete-dups (cdr lst))))
-      (with-temp-buffer
-        (prin1 lst (current-buffer))
-        (write-region (point-min) (point-max) ac-last-session-save-file)))
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (let ((cell (assq major-mode ac-last-session--candidates))
+              (words (split-string
+                      (buffer-substring-no-properties (point-min) (point-max))
+                      "\\(?:^\\|\\_>\\).*?\\(?:\\_<\\|$\\)")))
+          (if cell
+              (nconc (cdr cell) words)
+            (push (cons major-mode words) ac-last-session--candidates)))))
+    (dolist (pair ac-last-session--candidates)
+      (setcdr ac-last-session--candidates (delete-dups (cdr ac-last-session--candidates)))
+      (let ((pair (nthcdr (1- ac-last-session-saved-words) ac-last-session--candidates)))
+        (when pair (setcdr pair nil))))
+    (with-temp-buffer
+      (prin1 ac-last-session--candidates (current-buffer))
+      (write-region (point-min) (point-max) ac-last-session-save-file))
     (message "ac-last-session: session saved.")))
 
 (ac-define-source last-session
